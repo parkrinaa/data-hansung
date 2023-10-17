594bb6314d32b52f763d91282a41bf9c9fb31eeb
rina <flsk614@naver.com>
2023-10-17 22:40:42 +0900

    login페이지 변경

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -2,7 +2,18 @@
   <div class="login">
     <div class="inner">
       <div class="container">
-        <div class="logo animate__animated animate__pulse animate__infinite	infinite">한성대 앱 로고</div>
+        <div id="wrapper">
+          <div id="image">
+            <div class="image i1"></div>
+            <div class="image i2"></div>
+            <div class="image i3"></div>
+            <div class="image i4"></div>
+            <div class="image i5"></div>
+            <div class="image i6"></div>
+            <div class="image i7"></div>
+            <div class="image i8"></div>
+          </div>
+        </div>
         <div class="bottom">
           <h1>ICTDesign Department APP</h1>
           <span>Hansung university</span>
@@ -11,6 +22,7 @@
       </div>
     </div>
   </div>
+  
 </template>
 
 <script>

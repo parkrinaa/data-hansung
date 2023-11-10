42f2331440d1122a1e8c95290346b9daa307e144
rina <flsk614@naver.com>
2023-11-10 11:48:58 +0900

    오타수정

--- src/components/Product.vue
+++ src/components/Product.vue
@@ -16,7 +16,7 @@
       <div>
           <div class="c-modal" v-show="modal_3">
             <svg class="checkmark" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 52 52"><circle class="checkmark__circle" cx="26" cy="26" r="25" fill="none" /><path class="checkmark__check" fill="none" d="M14.1 27.2l7.1 7.2 16.7-16.8" /></svg>
-            <span>징바구니에 추가 되었습니다.</span>
+            <span>장바구니에 추가 되었습니다.</span>
             <div class="btns">
               <button  v-on:click="modal_3=!modal_3" class="modal-exit-btn">닫기</button>
               <router-link :to="{name: 'features'}">확인하기</router-link>
